4c6df183e4bb884d5c29dbab8c7c056df35d36b9
Mariia Paraskun <mparasku@c0c6c1.42berlin.de>
2023-06-22 17:28:11 +0200

    digit error handle

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,14 +6,26 @@
 /*   By: mparasku <mparasku@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 16:20:50 by mparasku          #+#    #+#             */
-/*   Updated: 2023/06/21 16:41:28 by mparasku         ###   ########.fr       */
+/*   Updated: 2023/06/22 17:27:39 by mparasku         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo.h"
+#include "../include/philo.h"
 
 int	main(int ac, char **av)
 {
-	if (ac == 2)
-		printf("Hello %s\n", av[1]);
+	if (ac == 5 || ac == 6) 
+	{
+		if (!(argm_parse(ac, av)))
+		{
+			error_print();
+			return (-1);
+		}
+		//printf("Hello %s\n", av[1]);
+	}
+	else
+	{
+		error_print();
+	}
+	return (-1);
 }
