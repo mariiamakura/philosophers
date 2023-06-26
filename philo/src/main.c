c824fd36274f701fb84159d6439bafe591deca34
mariiamakura <mariiaparaskun@gmail.com>
2023-06-26 20:10:27 +0200

    print struct added

--- philo/src/main.c
+++ philo/src/main.c
@@ -12,24 +12,38 @@
 
 #include "../include/philo.h"
 
+
+void print_struct(t_queue *queue)
+{
+    printf("%i\n", queue->philo_total_num);
+    printf("%lld\n", queue->time_to_died);
+    printf("%lld\n", queue->time_to_eat);
+    printf("%lld\n", queue->time_to_sleep);
+    printf("%i\n", queue->each_philo_eat);
+}
+
 int	main(int ac, char **av)
 {
     t_queue queue;
 
 	if (ac == 5 || ac == 6) 
 	{
-		if (!(argm_parse(ac, av)) || !init(ac, av, &queue))
+		if (!(argm_parse(ac, av)) || !init_data(ac, av, &queue))
 		{
 			error_print();
 			return (-1);
 		}
-	}
+        print_struct(&queue);
+    }
 	else
 	{
 		error_print();
         return (-1);
 	}
+
+
     destroy(&queue);
 }
+
 
 //run like this > MallocNanoZone=0 ./a.out
