bc6504f2c2c843ce016e17799e3c238e8380e84c
Mariia Paraskun <mparasku@c4c3c5.42berlin.de>
2023-06-27 15:09:14 +0200

    init_data change

--- philo/src/init_data.c
+++ philo/src/init_data.c
@@ -1,5 +1,6 @@
 #include "../include/philo.h"
-int init_data(int ac, char **av, t_queue *queue)
+
+t_queue *init_data(int ac, char **av, t_queue *queue)
 {
     queue->philo_total_num = (int) ft_atoi(av[1]);
     queue->time_to_died = ft_atoi(av[2]);
@@ -9,14 +10,15 @@ int init_data(int ac, char **av, t_queue *queue)
     if (ac == 6)
         queue->each_philo_eat = (int) ft_atoi(av[5]);
     if (queue->philo_total_num <= 0 || queue->philo_total_num > 200 || queue->time_to_died < 0 ||
+		queue->time_to_died > INT_MAX || queue->time_to_eat > MAX_INT || queue->time_to_sleep > MAX_INT ||
         queue->time_to_eat < 0 || queue->time_to_sleep < 0)
     {
-        return (0);
+        return (NULL);
     }
     queue->dead_philo = 0;
     if (pthread_mutex_init(&queue->queue_lock, NULL) != 0 || pthread_mutex_init(&queue->write_lock, NULL) != 0)
-        return (0);
-    return (1);
+        return (NULL);
+    return (queue);
 }
 
 void destroy(t_queue *queue)
